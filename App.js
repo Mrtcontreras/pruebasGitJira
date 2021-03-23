464047c8560dcaf47bb33e2827526da8cab84066
Marta <martacontrerasigualada@gmail.com>
2021-03-23 11:59:00 +0100

    cambios en css

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
 function darBienvenida() {
-    console.log("Hola Mundo, soy Marta");
+
+    document.getElementsByTagName("h3")[0].style.color = "red";
+    var nodo = document.createElement("h3");
+
+    if (document.getElementsByTagName("h3").length < 2) {
+        nodo.appendChild(document.createTextNode("Hola Mundo, soy Marta"));
+        console.log("Hola Mundo, soy Marta");
+    } else {
+        nodo.appendChild(document.createTextNode("Hola Mundo, soy Marta otra vez"));
+        console.log("Hola Mundo, soy Marta otra vez");
+    }
+
+    document.getElementsByTagName("h3")[0].parentNode.appendChild(nodo);
 }
